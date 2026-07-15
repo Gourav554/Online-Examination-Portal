048b18d831cfae44097ab56fbbf19d8fd8681522
Gourav554 <Jgourav554@gmail.com>
2026-07-15 23:56:40 +0530

    js

--- Result.js
+++ Result.js
@@ -1,4 +1,4 @@
-// Notification 
+//Notification 
 
 let notification = document.querySelector(".notification span");
 
@@ -6,7 +6,7 @@ notification.addEventListener("click", () => {
   notification.style.display = "none";
 });
 
-//  Dynamic Data
+//Dynamic Data
 
 const percentage = 80;
 
