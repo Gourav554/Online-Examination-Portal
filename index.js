7ec98e2babd29f7234f5a6c5a8978daf5ac45755
Gourav554 <Jgourav554@gmail.com>
2026-07-15 23:57:07 +0530

    js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function login() {
     let message =
         document.getElementById("message");
 
-    // Student Login
+    // Student Login id and password
     if (
         role === "student" &&
         id === "24571" &&
